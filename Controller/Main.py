90bb23f44f989426db3d2ce67cff325c59e08433
Sam Lightowler <slightowler7@gmail.com>
2019-12-15 02:45:15 +0000

    added voice functions but with some issues

--- Controller/Main.py
+++ Controller/Main.py
@@ -1,8 +1,23 @@
 import cv2
+# import bluetooth
+from gtts import gTTS
+from playsound import playsound
+import speech_recognition as sr
+import os
+import time
 
 
-def connect():
-    print("performing inquiry...")
+def connect_to_app():
+    print("Performing inquiry...")
+    nearby_devices = bluetooth.discover_devices(duration=8, lookup_names=True,
+                                                flush_cache=True, lookup_class=False)
+
+    print("Found {} devices".format(len(nearby_devices)))
+    for addr, name in nearby_devices:
+        try:
+            print("   {} - {}".format(addr, name))
+        except UnicodeEncodeError:
+            print("   {} - {}".format(addr, name.encode("utf-8", "replace")))
 
 
 def visualrec():
@@ -36,8 +51,32 @@ def detectMood():
     print("do something here")
 
 
+def speech_to_text():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        said = ""
+
+        try:
+            said = r.recognize_google(audio)
+            print(said)
+        except Exception as e:
+            print("Exception: " + str(e))
+
+    return said
+
+
+def text_to_speech():
+    tts = gTTS(text='test 123', lang='en')
+    audio = 'test.mp3'
+    tts.save(audio)
+    playsound(audio)
+
+
 def runApp():
-    print("logic")
+    print("do something here")
 
 
+text_to_speech()
+speech_to_text()
 visualrec()
